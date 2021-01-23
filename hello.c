f03cf48c08f1a755f813061bdcdd63f4757ccd21
github-ljw-github <qmail_ljw_qmail@foxmail.com>
2021-01-23 08:32:57 -0500

    add dtsi
    Signed-off-by: github-ljw-github <qmail_ljw_qmail@foxmail.com>

--- hello.c
+++ hello.c
@@ -2,16 +2,32 @@
 #include <linux/platform_device.h>
 #include <linux/gpio.h>
 #include <linux/of.h>
- 
+
+#define TAG "[hello]"
 static int hello_probe(struct platform_device *pdev)
 {
-    pr_notice("Driver probe : %s\n", __func__);
+    pr_notice(TAG "Driver probe : %s\n", __func__);
+
+    pr_notice(TAG "IORESOURCE_MEM:%x\n", platform_get_resource(pdev, IORESOURCE_MEM, 0));
+    pr_notice(TAG "IORESOURCE_MEM:%x\n", platform_get_resource(pdev, IORESOURCE_MEM, 1));
+    pr_notice(TAG "IORESOURCE_IRQ:%x\n", platform_get_resource(pdev, IORESOURCE_IRQ, 0));
+    pr_notice(TAG "IORESOURCE_REG:%x\n", platform_get_resource(pdev, IORESOURCE_REG, 0));
+
+
+    struct resource *mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if(mem)
+    {
+        pr_notice(TAG "mem->start: %x\n", mem->start);
+        pr_notice(TAG "mem->end: %x\n", mem->end);
+        pr_notice(TAG "mem->name: %s\n", mem->name);
+    }
+
     return 0;
 }
  
 static int hello_remove(struct platform_device *pdev)
 {
-    pr_notice("Driver remove :  %s\n", __func__);
+    pr_notice(TAG "Driver remove :  %s\n", __func__);
     return 0;
 }
  
@@ -33,4 +49,4 @@ static struct platform_driver hello_driver = {
  
 module_platform_driver(hello_driver);
 MODULE_AUTHOR("hello");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
